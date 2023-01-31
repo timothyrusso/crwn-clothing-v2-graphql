9e14238d255dcb3da54f353241f18201c1f3eabe
timothyrusso <russotimothy@live.it>
2023-01-31 18:52:23 +0100

    feat: add graphQL query for categories data

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,31 @@
 import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
+  console.log('Loading state', loading);
+  console.log('Data', data);
 
   const value = { categoriesMap };
   return (
